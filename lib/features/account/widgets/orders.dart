599e9db41fa7af256ff18442e7ce63432b95a2b6
vishnuprasad01 <vishnuprasad.mukundan@gmail.com>
2023-07-22 23:21:56 +0530

    single product view

--- lib/features/account/widgets/orders.dart
+++ lib/features/account/widgets/orders.dart
@@ -10,46 +10,52 @@ class Orders extends StatefulWidget {
 
 class _OrdersState extends State<Orders> {
 
+    List temporary = [
+        'https://images.unsplash.com/photo-1605360846332-1378e0c96955?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1hYyUyMGJvb2t8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+        'https://images.unsplash.com/photo-1510557880182-3d4d3cba35a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aXBob25lfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60',
+        'https://media.istockphoto.com/id/1366779817/photo/happy-young-man-using-virtual-reality-glasses.webp?b=1&s=170667a&w=0&k=20&c=JwuNXj5EXhK7RCKxc3UBWBFU_B-Ruhp4_mVG8ioXeeo='
+
+];
+
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                padding: const EdgeInsets.only(left: 10),
-                child: const Text(
-                  'Your Orders',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w600,
-                  ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 10),
+              child: const Text(
+                'Your Orders',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.only(right: 15),
-                child: Text(
-                  'See all',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: GlobalVariables.selectedNavBarColor,
-                  ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(right: 15),
+              child: Text(
+                'See all',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: GlobalVariables.selectedNavBarColor,
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
         //display the orders
         Container(
           height: 180,
           padding: const EdgeInsets.only(left: 10, right: 0, top: 20),
           child: ListView.builder(
-            itemCount: 1,
+            itemCount: 3,
             itemBuilder: (BuildContext context, int index) {
-              return ;
+              return;
             },
           ),
         )
---
+++ lib/features/account/widgets/single_product.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class SingleProduct extends StatelessWidget {
+  const SingleProduct
+({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
